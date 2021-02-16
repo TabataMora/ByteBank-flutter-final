53f433905c0514805a115e65662be3a9ebd630b5
Tabata Mora <50845464+TabataMora@users.noreply.github.com>
2021-02-16 15:00:08 -0300

    Configurando SQFLITE e criando o banco de dados do projeto.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 //import 'package:bytebank_final/screens/contacts_list.dart';
 //import 'package:bytebank_final/screens/contact_form.dart';
+import 'package:bytebank_final/database/app_database.dart';
+import 'package:bytebank_final/models/contact_model.dart';
 import 'package:bytebank_final/screens/dashboard.dart';
 //import 'package:bytebank_final/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(ByteBankApp());
+  save(Contact(0, 'Marlene', 1000)).then((id) {
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
 }
 
 class ByteBankApp extends StatelessWidget {
--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,32 +1,22 @@
+import 'package:bytebank_final/models/contact_model.dart';
 import 'package:bytebank_final/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Contatos'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Tabata',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactsItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -44,6 +34,32 @@ class ContactsList extends StatelessWidget {
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactsItem extends StatelessWidget {
+  final Contact contact;
+
+  const _ContactsItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
         ),
       ),
     );
