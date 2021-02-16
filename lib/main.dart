de560ef74d857ce58abb6b785b979644360aff7e
Tabata Mora <50845464+TabataMora@users.noreply.github.com>
2021-02-16 08:52:42 -0300

    Implementando a lista de contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+//import 'package:bytebank_final/screens/contacts_list.dart';
+//import 'package:bytebank_final/screens/contact_form.dart';
+import 'package:bytebank_final/screens/dashboard.dart';
+//import 'package:bytebank_final/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,56 +22,6 @@ class ByteBankApp extends StatelessWidget {
             textTheme: ButtonTextTheme.primary,
           )),
       home: Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Dashboard",
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).accentColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    Icons.people,
-                    size: 32.0,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
