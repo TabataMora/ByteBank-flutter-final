22eb678358f62aafbd13982f387b55b82118c636
Tabata Mora <50845464+TabataMora@users.noreply.github.com>
2021-03-01 06:33:13 -0300

    Configuando o interceptador do http

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,6 +1,23 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print(data);
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print(data);
+    return data;
+  }
+}
 
 void findAll() async {
-  final Response response = await get('192.168.0.14:8080/transactions');
-  print(response.body);
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
+  final Response response = await client.get('192.168.0.14:8080/transactions');
 }
