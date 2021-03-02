9397b71ac2fa9c295b0c5a60c9799a025bf825aa
Tabata Mora <50845464+TabataMora@users.noreply.github.com>
2021-03-02 06:22:57 -0300

    Realizando a conversão para json do objeto Dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:bytebank_final/screens/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:bytebank_final/http/webclient.dart';
 
 void main() {
   runApp(ByteBankApp());
+  findAll().then(
+    (transactions) => print('new transactions $transactions'),
+  );
 }
 
 class ByteBankApp extends StatelessWidget {
